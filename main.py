3ea874228d6212491ff36d99fb6989e031589d3e
Waseem Ahmad <waseem@rice.edu>
2012-12-17 17:20:44 -0600

    Completed website template and scaffolding.

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+"""
+The Rice Elections App.
+"""
+
+__author__ = 'Waseem Ahmad (waseem@rice.edu)'
+
+import jinja2
+import logging
+import os
 import webapp2
+
+MAIN_DIR = os.path.dirname(__file__)
+PAGES_DIR = os.path.join(MAIN_DIR, 'pages')
+
+JINJA_ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(PAGES_DIR))
+
+NAV_BAR = [
+    {'text': 'Home', 'link': 'index.html'},
+    {'text': 'Contact', 'link': 'contact.html'}]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        logging.info('Requested URL: %s', self.request.path)
+
+        # Get the page name being requested
+        # assume index.html if none specified
+        page_name = self.request.path
+        if page_name == '/':
+            page_name = NAV_BAR[0]['link']
+
+        # Get page info
+        try:
+            page = JINJA_ENV.get_template(page_name).render()
+        except Exception as e:
+            page = JINJA_ENV.get_template('not_found.html').render()
+
+        # Mark all links in the nav bar as inactive except the page open
+        for item in NAV_BAR:
+            if '/'+item['link'] == page_name:
+                item['active'] = True
+            else:
+                item['active'] = False
+
+        template = JINJA_ENV.get_template('template.html')
+        template_vals = {'nav_bar': NAV_BAR,
+                         'page_content': page}
+        self.response.out.write(template.render(template_vals))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/.*', MainHandler)
 ], debug=True)
