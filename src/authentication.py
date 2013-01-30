bececf5b3daa12893d12c05b4f68a701a265218c
capshaw <andrew.capshaw@gmail.com>
2013-01-30 13:23:10 -0600

    Logout: made logout message more friendly

--- src/authentication.py
+++ src/authentication.py
@@ -19,60 +19,60 @@ CAS_SERVER  = "https://netid.rice.edu"
 
 class LoginResponseHandler(webapp2.RequestHandler):
     """Receive the response from CAS after the user authentication."""
-    
+
     def get(self):
         ticket = self.request.get('ticket')
-        
+
         if not ticket:
             render_page(self, '/message', {'status': 'Error', 'msg': 'Ticket not specified.'})
             return
-        
+
         net_id = self.validate_cas_2()
         if not net_id:
             render_page(self, '/message', {'status': 'Error', 'msg': 'Invalid ticket. Credentials not verified.'})
             return
-        
+
         # Close any active session the user has since credentials have been freshly verified.
         session = get_current_session()
         if session.is_active():
             session.terminate()
-        
+
         # Get the user's record
         voter = database.get_voter(net_id, create=True)
-            
+
         # Start a session for the user
         session['net_id'] = voter.net_id
-        
+
         destination_url = str(self.request.get('destination'))
         if not destination_url:
             render_page(self, '/message', {'status': 'Error', 'msg': 'User authenticated. However, no destination '
                               'url is provided.'})
             return
-        
+
         logging.info('Redirecting to %s', destination_url)
         self.redirect(destination_url)
-                
+
     def validate_cas_2(self):
         """
         Validate the given ticket using CAS 2.0 protocol.
-        
+
         Returns:
             net_id {String}: the id of the user. None if ticket invalid.
         """
         ticket = self.request.get('ticket')
         service_url = self.remove_parameter_from_url(self.request.url, 'ticket')        # Strip ticket parameter
         cas_validate = CAS_SERVER + '/cas/serviceValidate?ticket=' + ticket + '&service=' + service_url
-        
+
         # Ask CAS server whether this ticket is valid
         f_validate = urllib.urlopen(cas_validate)
-        
+
         # Get the first line - should be yes or no
         response = f_validate.read()
         net_id = self.parse_tag(response, 'cas:user')
         if not net_id:
             logging.info('Invalid ticket.')
             return None
-        
+
         logging.info('Ticket validated for %s', net_id)
         return net_id
 
@@ -80,7 +80,7 @@ class LoginResponseHandler(webapp2.RequestHandler):
     def parse_tag(string, tag):
         """
         Used for parsing XML. Searches the string for first occurrence of <tag>...</tag>.
-        
+
         Returns:
             The trimmed text between tags. "" if tag is not found.
         """
@@ -92,12 +92,12 @@ class LoginResponseHandler(webapp2.RequestHandler):
         tag2_pos1 = string.find("</" + tag,tag1_pos2)
         if tag2_pos1==-1: return ""
         return string[tag1_pos2+1:tag2_pos1].strip()
-    
+
     @staticmethod
     def remove_parameter_from_url(url, parameter):
         """
         Removes the specified parameter from the url. Returns url as is if parameter doesn't exist.
-        
+
         Args:
             url {String}: input url
             parameter {String}: parameter to remove.
@@ -114,17 +114,17 @@ class LogoutHandler(webapp2.RequestHandler):
         service_url = 'http://%s/logout-response' % app_url
         self.redirect(CAS_SERVER + '/cas/logout?service=' + service_url)
 
-        
+
 class LogoutResponseHandler(webapp2.RequestHandler):
     def get(self):
         """Logs out the user."""
         session = get_current_session()
         if session.has_key('net_id'):
             session.terminate()
-            render_page(self, '/message', {'status': 'Done', 'msg': 'Logout complete.'})
+            render_page(self, '/message', {'status': 'Logged Out', 'msg': 'You\'ve been logged out. See you again soon!'})
         else:
             render_page(self, '/message', {'status': 'Silly', 'msg': 'You weren\'t logged in.'})
-                    
+
 def redirect_to_cas(request_handler):
     """
     Redirects client to CAS server for credentials verification.
@@ -146,7 +146,7 @@ def require_login(request_handler):
 def get_voter():
     """
     Returns the voter from user session.
-    
+
     Returns:
         voter: the Voter if authenticated. None otherwise.
     """
