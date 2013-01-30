c9e35c342a314d289d8a4af0e5bc7f62783cdb6e
capshaw <andrew.capshaw@gmail.com>
2013-01-30 12:40:44 -0600

    Vote.js: cleaned it up a little and documented.

--- src/js/vote.js
+++ src/js/vote.js
@@ -1,4 +1,3 @@
-
 var countdowns;
 var time_on_page;
 
@@ -7,14 +6,20 @@ var time_on_page;
  */
 $(document).ready(function() {
     startCountDownTimers()
-    setInterval(function(){ countdown(); }, 1000);
 });
 
+/**
+ * Starts the countdown timers going (updating every second)
+ */
 function startCountDownTimers() {
     countdowns = $('.countdown')
     time_on_page = 0;
+    setInterval(function(){ countdown(); }, 1000);
 }
 
+/**
+ * A function called once per second to update all countdown timers on the page.
+ */
 function countdown() {
     var time_left;
     time_on_page += 1000;
@@ -30,15 +35,15 @@ function countdown() {
     });
 }
 
+/**
+ * Helper function that returns a string timer from a millisecond input in hh:mm:ss format.
+ */
 function getTimerFromMilliseconds(millis){
     var hours = Math.floor(millis / 36e5),
         mins = Math.floor((millis % 36e5) / 6e4),
         secs = Math.floor((millis % 6e4) / 1000);
-    if (mins < 10)
-        mins = 0 + "" + mins;
-    if (secs < 10)
-        secs = 0 + "" + secs;
-    if (hours < 10)
-        hours = 0 + "" + hours;
+    if (mins < 10) mins = 0 + "" + mins;
+    if (secs < 10) secs = 0 + "" + secs;
+    if (hours < 10) hours = 0 + "" + hours;
     return hours+':'+mins+':'+secs
 }
