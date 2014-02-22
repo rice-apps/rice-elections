6c55e78726b89fc23edbaf7e7cf23e4a64205da1
Waseem Ahmad <waseem@rice.edu>
2014-02-22 18:02:43 -0500

    Better logging

--- src/authentication/auth.py
+++ src/authentication/auth.py
@@ -68,11 +68,11 @@ class LoginResponseHandler(webapp2.RequestHandler):
 
         # Get the first line - should be yes or no
         response = f_validate.read()
-        logging.info("CAS Authentication. Ticket: %s  Service-Url: %s  Server-Url: %s Response: %s",
+        logging.info("CAS Authentication.\nTicket: %s\nService-Url: %s\nServer-Url: %s\nResponse: %s",
                      ticket, service_url, cas_validate, response)
         net_id = self.parse_tag(response, 'cas:user')
         if not net_id:
-            logging.warning('Invalid ticket. Response: %s Service-Url: %s Server-Url: %s', 
+            logging.warning('Invalid ticket.\nResponse: %s\nService-Url: %s\nServer-Url: %s', 
                             response, service_url, cas_validate)
             return None
 
