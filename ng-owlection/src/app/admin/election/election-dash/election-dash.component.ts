80240a0761f7a3fecd840701cf0068029df5fd2e
Angela Hwang <hwangangela99@hotmail.com>
2018-01-27 15:42:18 -0600

    election skeleton

---
+++ ng-owlection/src/app/admin/election/election-dash/election-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-election-dash',
+  templateUrl: './election-dash.component.html',
+  styleUrls: ['./election-dash.component.css']
+})
+export class ElectionDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ng-owlection/src/app/admin/election/election-manage/positions/positions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positions',
+  templateUrl: './positions.component.html',
+  styleUrls: ['./positions.component.css']
+})
+export class PositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
