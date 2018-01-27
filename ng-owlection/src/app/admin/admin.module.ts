92b3e52a77110e474211ea99811b736c231c2d69
Angela Hwang <hwangangela99@hotmail.com>
2018-01-27 15:43:06 -0600

    admin module

--- ng-owlection/src/app/admin/admin.module.ts
+++ ng-owlection/src/app/admin/admin.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ElectionDashComponent } from './election-dash/election-dash.component';
+import { ElectionManageComponent } from './election-manage/election-manage.component';
+import { InfoComponent } from './election-manage/info/info.component';
+import { PositionsComponent } from './election-manage/positions/positions.component';
+import { VotersComponent } from './election-manage/voters/voters.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [ElectionDashComponent, ElectionManageComponent, InfoComponent, PositionsComponent, VotersComponent]
 })
 export class AdminModule { }
