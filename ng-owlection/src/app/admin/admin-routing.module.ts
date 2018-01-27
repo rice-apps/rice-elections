635a7523a22e1bdc2e295dcf3e9d69d32282337e
GaiSawant <grs7@rice.ed>
2018-01-27 16:22:19 -0600

    created admin routing module

--- ng-owlection/src/app/admin/admin-routing.module.ts
+++ ng-owlection/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ElectionDashComponent} from "./election/election-dash/election-dash.component";
+import {ElectionManageComponent} from "./election/election-manage/election-manage.component";
+import {InfoComponent} from "./election/election-manage/info/info.component";
+import {PositionsComponent} from "./election/election-manage/positions/positions.component";
+import {VotersComponent} from "./election/election-manage/voters/voters.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'election-dash',
+    component: ElectionDashComponent,
+  },
+  {
+    path: 'election-manage',
+    component: ElectionManageComponent,
+    children: [
+      {
+        path: 'info',
+        component: InfoComponent
+      },
+      {
+        path: 'positions',
+        component: PositionsComponent
+      },
+      {
+        path: 'voters',
+        component: VotersComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
