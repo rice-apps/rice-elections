35a3f7b3994c7b4f00311c9a632a6602f128707c
Savion Lee <harmonica1243@gmail.com>
2018-01-27 14:59:14 -0600

    Setup Skeleton for Modules
    Signed-off-by: Savion Lee <harmonica1243@gmail.com>

---
+++ ng-owlection/src/app/voter/voter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VoterRoutingModule } from './voter-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    VoterRoutingModule
+  ],
+  declarations: []
+})
+export class VoterModule { }
