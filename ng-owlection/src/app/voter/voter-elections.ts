2e9656900692c42fb95594c760cb31de11ac1652
DillonDoherty <ddoherty4913d@gmail.com>
2018-02-03 16:37:49 -0600

    I edited the voter model that is specific to the format of the file voter-landing-data.json in src/assets to add optional fields.

--- ng-owlection/src/app/voter/voter-elections.ts
+++ ng-owlection/src/app/voter/voter-elections.ts
@@ -1,8 +1,10 @@
 export class VoterElection {
   organization: string;
-  result_delay: number;
+  result_delay?: number;
   id: string;
   name: string;
-  end_date: string;
-  time_remaining: number
+  end_date?: string;
+  time_remaining?: number;
+  user_action?: string;
+  status: object;
 }
