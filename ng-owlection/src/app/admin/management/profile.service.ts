eac1c13c5021f720439ebd37757d8f74051a42d5
edwardfeng <jf44@rice.edu>
2018-02-03 16:31:36 -0600

    model added

--- ng-owlection/src/app/admin/management/profile.service.ts
+++ ng-owlection/src/app/admin/management/profile.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProfileService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
+  get_elections(): Observable<InternalsElection[]> {
+    return this.http.get<InternalsElection[]>('/assets/organization-admin-page-data.json');
+  }
 }
---
+++ ng-owlection/src/app/internals/models/orgAdminData.ts
@@ -0,0 +1,8 @@
+import {InternalsElection} from '../models/internals-election';
+
+export class OrgAdminData {
+  organizations: object;
+  active_org: object;
+  admins: object;
+  elections: InternalsElection[];
+}
