c2ff46c7c77b3f3a48d15b7c7a260ceb2dc92404
haleyko <haleyko@rice.edu>
2018-02-17 15:29:57 -0600

    temp add routing for admin

--- ng-owlection/src/app/admin/admin-routing.module.ts
+++ ng-owlection/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './management/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: ProfileComponent // TODO: will change to DashboardComponent later
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
