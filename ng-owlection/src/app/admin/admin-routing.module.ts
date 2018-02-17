523304a4cb768c8e14f02e63afce41e9400ed44a
haleyko <haleyko@rice.edu>
2018-02-17 16:23:31 -0600

    trying to make the buttons stay

--- ng-owlection/src/app/admin/admin-routing.module.ts
+++ ng-owlection/src/app/admin/admin-routing.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './management/profile/profile.component';
+import { MembersComponent } from './management/members/members.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'admin',
     component: DashboardComponent
+    children : [
+      { path: ''
+        component: DashboardComponent
+        children: [
+          {
+            path: 'profile',
+            component: ProfileComponent
+          },
+          {
+            path: 'members',
+            component: MembersComponent
+          }
+        ]
+      }
+    ]
   },
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
