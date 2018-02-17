805ef583fbd7d8c1beba74ccfe3842ac99653145
haleyko <haleyko@rice.edu>
2018-02-17 15:37:30 -0600

    add dashboard component

--- ng-owlection/src/app/admin/admin.module.ts
+++ ng-owlection/src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManagementModule } from './management/management.module';
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,6 @@ import { AdminRoutingModule } from './admin-routing.module';
     ManagementModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [DashboardComponent]
 })
 export class AdminModule { }
